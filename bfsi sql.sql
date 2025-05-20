16fd13d391b659be04c7ef0e9e36d78c3b594362
Eshwari-K <koppulaeshwari.2002@gmail.com>
2025-05-20 22:31:00 +0530

    Update bfsi sql.sql

--- bfsi sql.sql
+++ bfsi sql.sql
@@ -1,60 +1,61 @@
 show databases;
 use da;
+
+#Display the records whose ship mode is Economy and is greater than 25000
 select * from sales;
-/*select * from sales
 where ship_mode = "economy" 
 and total_amount >25000;
-*/
 
-/*select * from sales
+
+#Dispaly the records whose category is technology and country is Ireland after the 1stJan,2020.
+select * from sales
 WHERE Category = "Technology"
 and COUNTRY = "Ireland" and 
- STR_TO_DATE(Order_date, '%d-%m-%Y') > '2020-01-01'
- */
+STR_TO_DATE(Order_date, '%d-%m-%Y') > '2020-01-01'
  
- /*select * ,
+
+ #Dispaly Totalprofit in descending order with limit of 10
+ select * ,
  (Unit_Profit*Sold_Quantity) as "TotalProfit"
  from sales
  order by TotalProfit desc 
  limit 10;
-*/
 
-
-/*select * from sales
+#Dispaly customer names whose names Starts with J and ends with n
+select * from sales
 where customer_name like 'J%n'
-*/
 
-/*select * from sales
+
+#Display the product_name that has 'Acco' in it 
+select * from sales
 where  product_name
 having 'Acco';
-*/
-/*SELECT customer_name,product_name FROM sales
-WHERE product_name
-HAVING 'Acco';
-*/
+
+
 /*SELECT customer_name,product_name FROM sales
 Where product_name like 'Acco';
 */
 
-/*select city,total_amount from sales
+#Display the 5 highest total_amount,cities 
+select city,total_amount from sales
 order by total_amount desc
 limit 5;
-*/
 
-/*select sum(total_amount),sum(sold_quantity),avg(unit_cost) as 'Average_unitcost' 
+#Display total revenue,total sales ad average unit cost
+select sum(total_amount),sum(sold_quantity),avg(unit_cost) as 'Average_unitcost' 
 from sales
-*/
 
-/*select product_name,sum(sold_quantity) as 'total_sales'
+#Display product name and total_sales
+select product_name,sum(sold_quantity) as 'total_sales'
 from sales
 group by product_name
-*/
 
-/*select customer_name,total_amount from sales
+#Display customer names whose total_amount is greater than 50000
+select customer_name,total_amount from sales
 where total_amount>50000
-*/
 
-/*SELECT customer_name, total_amount AS sales_revenue
+#Display the total_revenue using Dense rank with limit of 5.
+SELECT customer_name, total_amount AS sales_revenue
 FROM (
   SELECT 
     customer_name,
@@ -63,4 +64,3 @@ FROM (
   FROM sales
 ) AS ranked_sales
 WHERE dr <= 5;
-*/
